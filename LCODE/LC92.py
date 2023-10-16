7b986613f782e4697ab438b6966cbd0d7915bc47
gaobaowo <15122665969@139.com>
2023-10-16 21:28:55 +0800

    LC92 边界有问题

--- LCODE/LC92.py
+++ LCODE/LC92.py
@@ -4,26 +4,33 @@ class ListNode:
         self.val = val
         self.next = next
 head=None
-dat=[1,2,4,3,5,9,1,2,4]
+dat=[9,8,7,6,5,4,3,2,1]
 for x in dat:
     b=ListNode(x,head)
     head=b
 a=head
-
+# 找到LEFT
 left=3-1
-right=7
+right=7-3
 while left>0:
     a=a.next
     left -=1
+#ll指向Left
 ll=a
+
 s=ListNode(a.val)
-while a.next !=None:
+ss=s
+while right>0:
     a=a.next
     c=ListNode(a.val,s)
     s=c
+    right -= 1
       
 ll.next=s
+ss.next=a
 
 while head.next!=None:
     print(head.val)
     head=head.next
+print(head.val)
+print(f'ss={ss.val}')
