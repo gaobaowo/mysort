01f614b0a7bc2079675d2ff9b7450a21e9a3fef9
gaobaowo <15122665969@139.com>
2023-10-19 13:45:30 +0800

    LC168 ok

--- LCODE/LC168.py
+++ LCODE/LC168.py
@@ -1,6 +1,16 @@
-x=701
-result=""
-while x!=0:
-    result=chr((x % 26)+64)+result
-    x//=26
-    print(x%26)
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = ""
+        x = columnNumber
+        while x != 0:
+            r = x % 26
+            x //= 26
+            if r ==0:
+                r = 26
+                x-=1
+            result = chr(r + 64) + result
+            
+            # print(f'r={r}   x={x}')
+        return result
+a=Solution()
+print(a.convertToTitle(1))
